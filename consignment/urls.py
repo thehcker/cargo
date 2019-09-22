1e04e393411f7f9401a869833357224ddde14409
ISAAC NYAKOI <isaacnyakoi1@gmail.com>
2019-09-22 03:24:48 +0300

    Add cargos

---
+++ consignment/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+	path('',views.cargo, name="cargo"),
+	path('extra/',views.extra, name="extra"),
+]
---
+++ consignment/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+
+def cargo(request):
+	context = {}
+	template = 'cargo.html'
+	return render(request, template, context)
+
+def extra(request):
+	context = {}
+	template = 'extra.html'
+	return render(request, template, context)
