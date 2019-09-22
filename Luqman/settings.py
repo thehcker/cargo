9f3f754dc2517d45388e15a02f7b852d56721bc1
ISAAC NYAKOI <isaacnyakoi1@gmail.com>
2019-09-22 11:49:25 +0300

    Add static

--- Luqman/settings.py
+++ Luqman/settings.py
@@ -119,3 +119,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+if DEBUG:
+    MEDIA_URL = '/media/'
+    STATIC_ROOT = os.path.join(BASE_DIR, "static", "static-only")
+    MEDIA_ROOT = os.path.join(BASE_DIR, "static", "media")
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, "static", "static"),
+    )
